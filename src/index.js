fab44c71fd89e3b534f974a36637eb66d132902e
BrianaHarper2022 <williamsbriana2022@gmail.com>
2024-02-15 21:25:52 -0500

    Added the AI API intergration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
+function displayPoem(response) {
+  
+
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 30,
+    cursor: "",
+  });
+}
+
 function generatePoem(event) {
     event.preventDefault();
 
-   new Typewriter("#poem", {
-     strings: "The poem will go here",
-     autoStart: true,
-     delay: 1,
-     cursor: "",
-   }); 
+    let instructionsInput = document.querySelector("#user-instructions");
+    let apiKey = "a94b5f688aoee508a9b7fca6t23274ef";
+    let prompt = `User instructions: Generate a 4 line poem about ${instructionsInput.value}.`;
+    let context =
+      "You are the master of poems. Generate a poem in basic HTML following the user instructions. Use <br /> after each line. The poem will NOT have titles. Sign the poems with `<br /><strong>SheCodes AI<strong>`";
+    let apiUrl =
+      `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+      axios.get(apiUrl).then(displayPoem); 
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
