bc69d6e121a080b95b1da42e8ad7dbe49df9c1b8
BrianaHarper2022 <williamsbriana2022@gmail.com>
2024-02-15 22:47:24 -0500

    Improved user experience

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 function displayPoem(response) {
   
-
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -19,6 +18,10 @@ function generatePoem(event) {
       "You are the master of poems. Generate a poem in basic HTML following the user instructions. Use <br /> after each line. The poem will NOT have titles. Sign the poems with `<br /><strong>SheCodes AI<strong>`";
     let apiUrl =
       `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+      let poemElement = document.querySelector("#poem");
+      poemElement.classList.remove("hidden");
+      poemElement.innerHTML = `<div class="blink">⏳ Generating a poem about ${instructionsInput.value}</div>`;
 
       axios.get(apiUrl).then(displayPoem); 
 }
